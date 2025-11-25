c1d539ff27dfdae0559d9cf8620932d3be2f524b
haitai0713 <haitai0713.mg14@nycu.edu.tw>
2025-11-25 14:38:36 +0800

    Update p1.cpp

--- src/p1.cpp
+++ src/p1.cpp
@@ -1,24 +1,21 @@
-// 題目 4：遞迴的足跡 (Footsteps of Recursion) - 30分
+// 題目 1：高塔建築師 (The Tower Architect) - 20分
 
 #include <iostream>
-#include <bits/stdc++.h>
 using namespace std;
 
-int fib(int n, bool call){
-    if(n != 1 && n != 2) return 0;
-    if((n == 1 && call == 0)||( n == 2 && call == 0)){
-        cout << "SEARCH fib(" << n << ")" << endl;
-        cout << "GET fib(" << n << ") = 1" << endl;
-    }
-    if((n == 1 && call == 1)||( n == 2 && call == 1)){
-        return 1;
-    }
+long long factorial(int n){
+    if (n == 1 || n == 0) return 1;
+    else return n * factorial(n-1);
+}
+
+long long sum(int n){
+    if(n == 0) return 0;
+    else return n + sum(n-1);
 }
 
 int main() {
     int n;
-    bool call = 1;
     cin >> n;
-    fib(n, 0);
-    cout << fib(n, 1) << endl;
+    cout << factorial(n) << endl;
+    cout << sum(n) << endl;
 }
