f8ea964370c1b316a424f511802b6a9c0a43b1a1
haitai0713 <haitai0713.mg14@nycu.edu.tw>
2025-11-25 14:35:14 +0800

    Update p1.cpp

--- src/p1.cpp
+++ src/p1.cpp
@@ -1,10 +1,24 @@
-// 題目 1：高塔建築師 (The Tower Architect) - 20分
+// 題目 4：遞迴的足跡 (Footsteps of Recursion) - 30分
 
 #include <iostream>
+#include <bits/stdc++.h>
 using namespace std;
 
+int fib(int n, bool call){
+    if(n != 1 && n != 2) return 0;
+    if((n == 1 && call == 0)||( n == 2 && call == 0)){
+        cout << "SEARCH fib(" << n << ")" << endl;
+        cout << "GET fib(" << n << ") = 1" << endl;
+    }
+    if((n == 1 && call == 1)||( n == 2 && call == 1)){
+        return 1;
+    }
+}
+
 int main() {
-    
-    
-    return 0;
+    int n;
+    bool call = 1;
+    cin >> n;
+    fib(n, 0);
+    cout << fib(n, 1) << endl;
 }
